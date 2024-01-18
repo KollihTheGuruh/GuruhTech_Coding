64c4721b83ecd1fc1f257173654cf2fe955f29c6
KollihTheGuruh <mmike8449@gmail.com>
2024-01-18 03:29:51 +0300

    spiral4

--- spiral4.py
+++ spiral4.py
@@ -1,36 +1,42 @@
 import turtle
 
-# Function to create a branch of the fractal tree with flowers at the branches
-def draw_branch(turtle, branch_length, level):
+# Function to set the flower color based on the angle
+def set_flower_color(turtle, angle):
+    if 0 <= angle < 90:
+        turtle.color("yellow")
+    elif 90 <= angle < 180:
+        turtle.color("green")
+    elif 180 <= angle < 270:
+        turtle.color("pink")
+    else:
+        turtle.color("white")
+
+# Function to create a branch of the fractal tree with flowers
+def draw_branch(turtle, branch_length, level, angle):
     if level <= 0:
-        # Draw a colored dot for the flower
-        heading = turtle.heading()
-        if 0 <= heading < 90:
-            turtle.dot(4, "yellow")
-        elif 90 <= heading < 180:
-            turtle.dot(4, "green")
-        elif 180 <= heading < 270:
-            turtle.dot(4, "pink")
-        else:
-            turtle.dot(4, "white")
+        set_flower_color(turtle, angle)
+        turtle.dot()  # Draw a colored dot representing the flower
+        turtle.color("white")  # Reset color for the branches
     else:
         turtle.forward(branch_length)
         turtle.left(45)
-        draw_branch(turtle, branch_length / 2, level - 1)
+        draw_branch(turtle, branch_length / 2, level - 1, angle)
         turtle.right(90)
-        draw_branch(turtle, branch_length / 2, level - 1)
+        draw_branch(turtle, branch_length / 2, level - 1, angle)
         turtle.left(45)
         turtle.backward(branch_length)
 
 # Function to create a full 360-degree fractal tree pattern
 def draw_fractal_forest(turtle, branch_length, level, num_trees):
-    angle = 360 / num_trees
+    angle_increment = 360 / num_trees
+    angle = 0
     for _ in range(num_trees):
-        draw_branch(turtle, branch_length, level)
-        turtle.left(angle)
+        draw_branch(turtle, branch_length, level, angle)
+        turtle.left(angle_increment)
+        angle += angle_increment
 
 # Set up the screen and turtle
-turtle.speed(0)
+turtle.speed(30)
 turtle.hideturtle()
 turtle.bgcolor("black")
 turtle.color("white")
